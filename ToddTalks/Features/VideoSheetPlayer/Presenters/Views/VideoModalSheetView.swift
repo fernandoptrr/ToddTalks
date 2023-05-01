b3d1fc9a77d47b18cad8d19ab72e2c5812f49e8a
Fernando Putra <fernnandoptr@gmail.com>
2023-05-01 19:13:50 +0700

    resolve conflict pr #4

---
+++ ToddTalks/Features/VideoSheetPlayer/Presenters/Views/VideoModalSheetView.swift
@@ -0,0 +1,79 @@
+//
+//  VideoModalSheetView.swift
+//  ToddTalks
+//
+//  Created by Angelica Pinonkuan on 25/04/23.
+//
+
+import SwiftUI
+import AVKit
+import WebKit
+
+struct VideoModalSheetView: View {
+    
+    @State private var showModal = false
+    
+    var body: some View {
+        
+        Button {
+            showModal.toggle()
+        } label: {
+            PlayVideoButton()
+        }
+        .fullScreenCover(isPresented: $showModal, content: FullScreenModalView.init)
+    }
+}
+
+struct VideoModalSheetView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoModalSheetView()
+    }
+}
+
+struct FullScreenModalView: View {
+ 
+    @Environment(\.presentationMode) var presentationMode
+     
+    var body: some View {
+        
+        VStack{
+            
+            Button{
+                presentationMode.wrappedValue.dismiss()
+            } label: {
+                HStack{
+                    Image(systemName: "chevron.left")
+                    Text("Back")
+                    Spacer()
+                }
+            }
+            .padding()
+            .foregroundColor(.blue)
+            
+            VideoPlayer(player: AVPlayer(url: Bundle.main.url(forResource: "mamaSong", withExtension: "mp4")!))
+                .ignoresSafeArea()
+            //        YouTubeView(videoID: "73e3GNxcKvA")
+            //            .ignoresSafeArea()
+             
+           
+        }
+        
+    }
+}
+
+// Video from Youtube Video
+struct YouTubeView: UIViewRepresentable {
+    let videoID: String
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let embedURL = "https://www.youtube.com/embed/\(videoID)"
+        let urlRequest = URLRequest(url: URL(string: embedURL)!)
+        uiView.load(urlRequest)
+    }
+}
+
+
