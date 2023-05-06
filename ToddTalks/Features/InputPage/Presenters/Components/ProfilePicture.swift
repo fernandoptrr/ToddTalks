00084bd58226d9efbf46fd6ceb74f8b72e533b9b
angelicapinonkuan <angelicapinonkuan@gmail.com>
2023-05-06 19:39:42 +0700

    adding action sheet pop over to choose between accessing camera or photo library

--- ToddTalks/Features/InputPage/Presenters/Components/ProfilePicture.swift
+++ ToddTalks/Features/InputPage/Presenters/Components/ProfilePicture.swift
@@ -9,40 +9,45 @@ import SwiftUI
 
 struct ProfilePicture: View {
     
-    @State var changeProfileImage = false
-    @State var openCameraRoll = false
-    @State var imageSelected = UIImage()
+    @State private var showSheet: Bool = false
+    @State private var showImagePicker: Bool = false
+    @State private var sourceType: UIImagePickerController.SourceType = .camera
+    @State private var image: UIImage?
     
     var body: some View {
         
-        ZStack(alignment: .bottomTrailing) {
-            Button {
-                changeProfileImage = true
-                openCameraRoll = true
-                        
-            } label: {
-                if changeProfileImage {
-                    Image(uiImage: imageSelected)
+            VStack {
+                Button {
+                    self.showSheet = true
+                } label: {
+                    Image(uiImage: image ?? UIImage(named:"profilepic")!)
                         .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 200, height: 200)
+                        .foregroundColor(.gray)
                         .clipShape(Circle())
-                        .frame(width: 200)
-                } else {
-                    ZStack{
-                        Circle()
-                            .frame(width: 200)
-                            .foregroundColor(.gray)
-                        Image(systemName: "camera")
-                            .font(.system(size: 60))
-                            .foregroundColor(.white)
-                    }
+                    
+                    
                 }
-            }
-                   
-        }.sheet(isPresented: $openCameraRoll) {
-            ImagePicker(selectedImage: $imageSelected, sourceType: .camera)
+                .padding()
+                    .actionSheet(isPresented: $showSheet) {
+                        ActionSheet(title: Text("Pilih Foto"), buttons: [
+                            .default(Text("Pilih Dari Galeri")) {
+                                self.showImagePicker = true
+                                self.sourceType = .photoLibrary
+                            },
+                            .default(Text("Ambil Gambar")) {
+                                self.showImagePicker = true
+                                self.sourceType = .camera
+                            },
+                            .cancel(Text("Batalkan"))
+                        ])
+                }
+              
+            
+        }.sheet(isPresented: $showImagePicker) {
+            ImagePicker(image: self.$image, isShown: self.$showImagePicker, sourceType: self.sourceType)
         }
-        
-        
     }
 }
 
